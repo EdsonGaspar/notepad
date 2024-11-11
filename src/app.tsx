8ca1ef8dacfa90b43fcd1c07b3371250dc75d445
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-11-11 14:06:01 +0100

    Feat: Add imagens, add logo e criação dos components

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
+import logo from "./assets/logo-nlw-expert.svg";
 import "./index.css";
 
 export function App() {
   return (
-    <>
-      <h1 className="text-sm">NotePad</h1>
-    </>
+    <div>
+      <img src={logo} alt="Logo" />
+      <h1>Mostra logo</h1>
+    </div>
   );
 }
