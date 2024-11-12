418b7c4f11377c35dcbf4702301573e3e5f8fc89
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-11-12 20:34:21 +0100

    Feat: Card criado e clicavel, chamando um modal com a sua estilização...

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,3 +1,4 @@
+import * as Dialog from "@radix-ui/react-dialog";
 interface NoteCardProps {
   note: {
     date: Date;
@@ -5,15 +6,29 @@ interface NoteCardProps {
   };
 }
 
-export function NoteCard(props: NoteCardProps) {
+export function NoteCard({ note }: NoteCardProps) {
   return (
-    <button className="bg-slate-800 flex flex-col text-left rounded-md p-5 gap-3 outline-none overflow-hidden relative hover:ring-2 hover: ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-      <span className="text-sm font-medium text-slate-300">
-        {props.note.date.toISOString()}{" "}
-      </span>
-      <p className="text-slate-400 text-sm leading-6 ">{props.note.content}</p>
+    <Dialog.Root>
+      <Dialog.Trigger className="bg-slate-800 flex flex-col text-left rounded-md p-5 gap-3 outline-none overflow-hidden relative hover:ring-2 hover: ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+        <span className="text-sm font-medium text-slate-300">
+          {note.date.toISOString()}{" "}
+        </span>
+        <p className="text-slate-400 text-sm leading-6 ">{note.content}</p>
 
-      <div className="absolute bottom-0 left-0 right-0 h-1/2  bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"></div>
-    </button>
+        <div className="absolute bottom-0 left-0 right-0 h-1/2  bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"></div>
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
+        <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+          <div className="flex flex-1 flex-col gap-3 p-5">
+            <span className="text-sm font-medium text-slate-300">
+              {note.date.toISOString()}{" "}
+            </span>
+            <p className="text-slate-400 text-sm leading-6 ">{note.content}</p>
+          </div>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
